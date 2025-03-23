eae30c1d6e8f4f4c74576c2cbbe7d0406b71c81c
Dinja8345 <pocchi8345@gmail.com>
2025-03-23 21:16:11 +0900

    ログイン時にsidをcookie,DBにセーブできるように

--- src/app/users/login/actions/loginUser.ts
+++ src/app/users/login/actions/loginUser.ts
@@ -1,33 +1,35 @@
-"use server"
+"use server";
 
-import connectDB from '@/lib/mongodb';
-import axios from 'axios';
-import User from '@/models/User';
-import verifyPassword from '@/app/users/api/verifyPassword';
+import crypto from "crypto";
+import axios from "axios";
 
-export default async function loginUser(state: any, formData: FormData){
-    try{
-        await connectDB();
-        const email = formData.get("email") as string;
-        const inputedPass = formData.get("password") as string;
-        const storedUsers = await User.findOne({ email: email })
-        if(!storedUsers){
-            return { msg: "そのメールアドレスは登録されていません" }
-        }
-        const storedPass = storedUsers.password
-    
-        const isMatch: boolean = await verifyPassword(inputedPass,storedPass);
-        if(isMatch){
-            const data = { email };
-            const res = await axios.get("http://localhost:3000/api/session");
-            console.log(res.data);
-            return { msg: "ログインに成功" };
-        }else{
-            return { msg: "メールアドレスまたはパスワードが一致していません" };
-        }
-    }catch(e){
-        console.error(e);
-        return { msg: "ログイン中にエラーが発生しました。" }
+import connectDB from "@/lib/mongodb";
+import User from "@/models/User";
+import verifyPassword from "@/app/users/api/verifyPassword";
+
+export default async function loginUser(state: any, formData: FormData) {
+  try {
+    await connectDB();
+    const email = formData.get("email") as string;
+    const inputedPass = formData.get("password") as string;
+    const storedUsers = await User.findOne({ email: email });
+    if (!storedUsers) {
+      return { msg: "そのメールアドレスは登録されていません" };
     }
-}
+    const storedPass = storedUsers.password;
 
+    const isMatch: boolean = await verifyPassword(inputedPass, storedPass);
+    if (isMatch) {
+      const sid = crypto.randomBytes(32).toString("hex");
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/session`, { sid });
+      const dbRes = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/MongoDB/sessionStore`, { sid, email });
+      //console.log(res.data);
+      return { msg: "ログインに成功" };
+    } else {
+      return { msg: "メールアドレスまたはパスワードが一致していません" };
+    }
+  } catch (e) {
+    console.error(e);
+    return { msg: "ログイン中にエラーが発生しました。" };
+  }
+}
